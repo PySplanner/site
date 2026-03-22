6b229919deb7c57e4157d4a039d5bf258963663a
DylDev <dylanb92010@gmail.com>
2026-03-22 15:35:21 -0400

    Use ./ instead of / on images

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
         <div className="relative flex flex-col w-full h-full justify-center items-center">
             <div className="absolute top-0 left-0 right-0 h-[30vh] bg-linear-to-b from-primary/4 to-transparent pointer-events-none" />
 
-            <img className="rounded-md mt-10 mb-6" width={570} height={240} src="/banner.png" alt="PySplanner Logo" />
+            <img className="rounded-md mt-10 mb-6" width={570} height={240} src="./banner.png" alt="PySplanner Logo" />
             <div className="w-1/2 text-center">
                 <TypedHeading />
                 <p className="text-lg text-muted-foreground mt-4">PySplanner is a powerful tool that generates optimized movement plans for LEGO robots, ensuring efficient and effective performance.</p>
